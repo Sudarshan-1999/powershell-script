70a78cf4a32500988b0d1185fc1aaeb13ac54d5e
Sudarshan Damahe <52589902+Sudarshan-1999@users.noreply.github.com>
2023-05-31 15:20:40 +0530

    Update custominstallqt.ps1

--- custominstallqt.ps1
+++ custominstallqt.ps1
@@ -1,6 +1,6 @@
 $installerPath = "C:\Users\SudarshanRamdasDamah\Downloads\qt-unified-windows-x64-4.5.2-online.exe"
-$email = "jenkins@rampgroup.com"
-$password = "Welcome@Ramp"
+$email = "#######@###.###"
+$password = "*******"
 $components = "qt.tools.win64_msvc2019", "qt.qt5.15.2.win64_msvc2019_64"
 
 # Set the options for the installer
